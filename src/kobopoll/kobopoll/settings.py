d59eee5fd2c692bf5962def3a10e0c05e5cbe598
Udemezue John <johnzeus14@gmail.com>
2026-09-12 09:06:48 +0100

    added dont env

--- src/kobopoll/kobopoll/settings.py
+++ src/kobopoll/kobopoll/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tol%@v#cukne$z1g#ui!e4y%6u3f!^)o%**un_b)po!jq%ito7'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
